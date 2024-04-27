2cdbc7d60399ac0e41d7c0f655f86cbf65a7c051
Ben Yip <ben_yip@126.com>
2024-04-27 22:06:20 +0800

    Add asset folder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+
+import images from "../assets";
 
 const Navbar = () => {
   return <nav>Navbar</nav>;
