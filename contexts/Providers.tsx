c43cc5e3ffbf515b04dd7f83e560bcf2c40bdb36
Ben Yip <ben_yip@126.com>
2024-04-27 21:52:16 +0800

    Add fontawesome script source

--- contexts/Providers.tsx
+++ contexts/Providers.tsx
@@ -1,9 +1,18 @@
 "use client";
-import { ThemeProvider } from "next-themes"; // This should be the Redux provider
+import { ThemeProvider } from "next-themes";
 import { PropsWithChildren } from "react";
+import Script from "next/script";
 
 const Providers: React.FC<PropsWithChildren> = ({ children }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class">
+      {children}
+      <Script
+        src="https://kit.fontawesome.com/5f7ac22047.js"
+        crossOrigin="anonymous"
+      ></Script>
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
