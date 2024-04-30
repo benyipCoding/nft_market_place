852f40e4465f45784a955d1e82377af9c66993eb
Ben Yip <ben_yip@126.com>
2024-04-30 23:18:59 +0800

    Add CreatorCard list

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,47 @@
 'use client';
 import { Banner, CreatorCard } from '@/components';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+import images from '@/assets';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 const Home = () => {
   const parentRef = useRef<HTMLDivElement>(null);
   const scrollRef = useRef<HTMLDivElement>(null);
+
+  const { theme } = useTheme();
+
+  const handleScroll = (direction: 'left' | 'right') => {
+    const { current } = scrollRef;
+    if (!current) return;
+
+    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+    if (direction === 'left') {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
+  const handleHorizontalScroll = (e: WheelEvent) => {
+    const { current } = scrollRef;
+    if (!current) return;
+    if (current.scrollLeft === 0 && e.deltaY < 0) return;
+    if (
+      current.offsetWidth + current.scrollLeft >= current.scrollWidth &&
+      e.deltaY > 0
+    )
+      return;
+    e.preventDefault();
+    scrollRef.current!.scrollLeft += e.deltaY * 10;
+  };
+
+  useEffect(() => {
+    const scrollDiv = document.querySelector('#scrollDiv');
+    // @ts-ignore
+    scrollDiv!.addEventListener('wheel', handleHorizontalScroll);
+  }, []);
 
   return (
     <div className="flex justify-center sm:px-4 p-12">
@@ -23,10 +60,45 @@ const Home = () => {
             <div
               className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
               ref={scrollRef}
+              id="scrollDiv"
             >
               {[6, 7, 8, 9, 10].map((i) => (
-                <CreatorCard />
+                <CreatorCard
+                  key={`creator-${i}`}
+                  rank={i}
+                  // @ts-ignore
+                  creatorImage={images[`creator${i}`]}
+                  creatorName={`0xabc...efgd`}
+                  creatorEths={10 - i * 0.5}
+                />
               ))}
+
+              <>
+                <div
+                  className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0"
+                  onClick={() => handleScroll('left')}
+                >
+                  <Image
+                    src={images.left}
+                    layout="fill"
+                    objectFit="contain"
+                    alt="left_arrow"
+                    className={`${theme === 'light' && 'filter invert'}`}
+                  />
+                </div>
+                <div
+                  className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0"
+                  onClick={() => handleScroll('right')}
+                >
+                  <Image
+                    src={images.right}
+                    layout="fill"
+                    objectFit="contain"
+                    alt="left_arrow"
+                    className={`${theme === 'light' && 'filter invert'}`}
+                  />
+                </div>
+              </>
             </div>
           </div>
         </div>
